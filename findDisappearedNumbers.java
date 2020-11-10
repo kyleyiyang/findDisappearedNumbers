b87fe1912f2e6a3732b49f009e31d6e993aaa9f0
kyleyiyang <kyleyiyang@gmail.com>
2020-11-10 17:50:17 -0500

    Update findDisappearedNumbers.java

--- findDisappearedNumbers.java
+++ findDisappearedNumbers.java
@@ -10,3 +10,24 @@ class Solution {
         return ans;
     }
 }
+
+//Still slow
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> ans = new ArrayList<>(4);
+        for (int i=1;i<nums.length+1;i++) {
+            //ans.add(nums[i-1],new Integer(nums[i-1]));
+            ans.add(i);
+            if (!ans.contains(nums[i-1])) {
+                //ans.add(nums[i-1],nums[i-1]);
+            }
+        }
+        ans.removeIf(n -> (IntStream.of(nums).anyMatch(x -> x == n)));
+        /*System.out.println(ans.size());
+        for (int n:nums) {
+            ans.set(n-1,n);
+            //ans.remove(new Integer(n));
+        }*/
+        return ans;
+    }
+}
